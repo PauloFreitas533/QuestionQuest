9d1f7ca80f3c41c61ca8651ecf0ab0d35a9d2b3a
PauloFreitas533 <paulinho.f533@gmail.com>
2023-05-10 22:30:40 -0300

    Adjusting the closing of the screen when you press the escape button

--- Script/DialogControl.cs
+++ Script/DialogControl.cs
@@ -10,15 +10,61 @@ public class DialogControl : MonoBehaviour
     public Image profile;
     public Text speakText;
     public Text actorNameText;
+    public Text answerText1;
+    public Text answerText2;
+    public Text answerText3;
+    public Text answerText4;
+    //public Button Btn-Escape;
 
     [Header("Settings")]
     public float typingSpeed;
+    //private string sentences;
+    //private int index;
 
-    public void Speak(Sprite p, string txt, string actorName)
+    public void Speak(Sprite p, string txt, string actorName, string answer1, string answer2, string answer3, string answer4)
     {
         dialogObj.SetActive(true);
         profile.sprite = p;
         speakText.text = txt;
         actorNameText.text = actorName;
+        answerText1.text = answer1;
+        answerText2.text = answer2;
+        answerText3.text = answer3;
+        answerText4.text = answer4;
+        //StartCoroutine(TypeSentence());
+    }
+
+    //IEnumerator TypeSentence()
+    //{
+    //    foreach (char letter in sentences[index].ToCharArray())
+    //    {
+    //        speakText.text += letter;
+    //        yield return new WaitForSeconds(typingSpeed);
+    //    }
+    //}
+
+    //public void NextSentence()
+    //{
+    //    if(speakText.text == sentences[index])
+    //    {
+    //        if(index < sentences.Length - 1)
+    //        {
+    //            index++;
+    //            speakText.text = "";
+    //            StartCoroutine(TypeSentence());
+    //        }
+    //        else
+    //        {
+    //            speakText.text = "";
+    //            index = 0;
+    //            dialogObj.SetActive(false);
+    //        }
+    //    }
+    //}
+
+    public void Escape()
+    {
+        dialogObj.SetActive(false);
+        Time.timeScale = 1f;
     }
 }
--- Script/DialogGhost.cs
+++ Script/DialogGhost.cs
@@ -8,15 +8,22 @@ public class DialogGhost : MonoBehaviour
     public Sprite profile;
     public string speakTxt;
     public string actorName;
+    public string answerText1;
+    public string answerText2;
+    public string answerText3;
+    public string answerText4;
 
     public LayerMask playerLayer;
     public float radious;
 
     private DialogControl dc;
-    public bool isDialogActive = false;
+    private bool onRadious;
+    private float originalRadious;
+    private bool isCoroutineRunning = false;
 
     private void Start()
     {
+        originalRadious = radious;
         dc = FindObjectOfType<DialogControl>();
     }
 
@@ -25,24 +32,44 @@ public class DialogGhost : MonoBehaviour
          Interact();
     }
 
+    private void Update()
+    {
+        if (onRadious)
+        {
+            dc.Speak(profile, speakTxt, actorName, answerText1, answerText2, answerText3, answerText4);
+            onRadious = false;
+        }
+    }
+
     public void Interact()
     {
         Collider2D hit = Physics2D.OverlapCircle(transform.position, radious, playerLayer);
-        
-        if(hit != null)
+
+        if (hit != null)
         {
-            isDialogActive = true;
-            if (isDialogActive)
+            onRadious = true;
+            radious = 0;
+            Time.timeScale = 0f;
+
+            if (!isCoroutineRunning)
             {
-            dc.Speak(profile, speakTxt, actorName);
-                Time.timeScale = 0f;
-            }
-            else
-            {
-                isDialogActive = false;
-                Time.timeScale = 1f;
+                StartCoroutine(RestoreRadious());
             }
         }
+        else
+        {
+            onRadious = false;
+            Time.timeScale = 1f;
+        }
+    }
+
+    private IEnumerator RestoreRadious()
+    {
+        isCoroutineRunning = true;
+        yield return new WaitForSeconds(2f);
+
+        radious = originalRadious;
+        isCoroutineRunning = false;
     }
 
     private void OnDrawGizmosSelected()
